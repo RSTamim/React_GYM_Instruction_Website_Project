02cf519eecc2066d07b193ffbf045199f82894da
Md. Tamim Hasan <59114274+RSTamim@users.noreply.github.com>
2022-10-11 19:00:10 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-          <span style={{color: 'white'}}>HI! I am Tamim Hasan as a React Learner</span>
+          <span style={{color: 'white'}}>HI! I am Tamim Hasan as a React Developer</span>
     </div>
   );
 }
